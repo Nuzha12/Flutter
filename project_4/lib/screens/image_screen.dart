dcfbfd909d72329a7fe7f0b9e5e156353e3128be
Fathima Nuzha <fathimanusha1998836@gmail.com>
2026-02-20 16:39:29 +0530

    project_4 added

---
+++ project_4/lib/screens/image_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class ImageScreen extends StatefulWidget {
+  const ImageScreen({super.key});
+
+  @override
+  State<ImageScreen> createState() => _ImageScreenState();
+}
+
+class _ImageScreenState extends State<ImageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Center(
+            child: Image.asset('assets/img/wallet.png',  fit: BoxFit.fitWidth),
+          ),
+
+          Center(
+            child: Image.network("https://cdn-icons-png.flaticon.com/512/3135/3135715.png", height: 300,
+            loadingBuilder: (context, child, loadingProgress){
+              if (loadingProgress == null) return child;
+              return Center(
+                  child: const CircularProgressIndicator(),
+              );
+            }
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
